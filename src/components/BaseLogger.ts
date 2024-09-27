33a70e7df37850699b447bb511dd1595301f1829
srdn2417 <srdn2417@gmail.com>
2024-09-27 13:40:37 -0400

    fix: update configuration for typescript

--- src/components/BaseLogger.ts
+++ src/components/BaseLogger.ts
@@ -1,5 +1,7 @@
+import { injectable } from 'inversify';
 import { Logger } from '../interfaces';
 
+@injectable()
 export class BaseLogger implements Logger {
   info(msg: string, data: object): void {
     console.log(`${this.constructor.name} (INFO): `, msg, JSON.stringify(data));
